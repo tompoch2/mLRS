99c3f355a62207eefed85705c50f0537fc8369ff
olliw42 <waldmanns@gmx.de>
2024-08-17 06:17:59 +0200

    esp, tx, adapt eps-i2c lib and gdisp.c such that it works also for NTF1115 displays (thx brad112358)

--- mLRS/Common/esp-lib/esp-i2c.h
+++ mLRS/Common/esp-lib/esp-i2c.h
@@ -70,45 +70,80 @@ IRAM_ATTR HAL_StatusTypeDef i2c_device_ready(void)
 }
 */
 
-struct {
+#define I2C_WORK_CHUNK_SIZE 128
+#define I2C_WORK_QUEUE_SIZE 16
+
+typedef struct
+{
+    uint8_t buf[I2C_WORK_CHUNK_SIZE];
     uint8_t reg_adr;
     uint16_t pos;
-    uint16_t len = 0;
-} i2c_buf;
-uint8_t i2c_buffer[1024];
+    uint16_t len;
+} i2c_chunk_t;
+
+struct
+{
+    i2c_chunk_t queue[I2C_WORK_QUEUE_SIZE];
+    uint8_t next;
+    uint8_t end;
+} i2c_work;
 
 
 IRAM_ATTR HAL_StatusTypeDef i2c_put(uint8_t reg_adr, uint8_t* buf, uint16_t len)
 {
-    i2c_buf.reg_adr = reg_adr;
-    i2c_buf.len = len;
-    i2c_buf.pos = 0;
-    memcpy(i2c_buffer, buf, i2c_buf.len);
-    return HAL_OK;
+    if (i2c_work.end < I2C_WORK_QUEUE_SIZE) {  // not work overflow
+        i2c_chunk_t* i2c_buf = i2c_work.queue + i2c_work.end;
+        if (len <= I2C_WORK_CHUNK_SIZE) { // not buf overflow
+            i2c_buf->reg_adr = reg_adr;
+            memcpy(i2c_buf->buf, buf, len);
+            i2c_buf->len = len;
+            i2c_buf->pos = 0;
+            i2c_work.end++;
+            return HAL_OK;
+        }
+    }
+    return HAL_ERROR;
 }
 
 
 IRAM_ATTR void i2c_spin(uint16_t chunksize)
 {
-    if (!i2c_buf.len) return; // nothing to do
+    if (i2c_work.end == i2c_work.next) return; // nothing to do
 
-    if (i2c_buf.pos + chunksize > i2c_buf.len) chunksize = i2c_buf.len - i2c_buf.pos;
+    while (true) {
+        i2c_chunk_t* i2c_buf = i2c_work.queue + i2c_work.next;
+        uint16_t writesize = chunksize;
 
-    if (chunksize) {
-        Wire.beginTransmission(i2c_dev_adr);
-        Wire.write(i2c_buf.reg_adr);
-        Wire.write(&(i2c_buffer[i2c_buf.pos]), chunksize);
-        Wire.endTransmission(true);
+        if (writesize > i2c_buf->len - i2c_buf->pos) writesize = i2c_buf->len - i2c_buf->pos;
+
+        if (writesize) {
+            Wire.beginTransmission(i2c_dev_adr);
+            Wire.write(i2c_buf->reg_adr);
+            Wire.write(&(i2c_buf->buf[i2c_buf->pos]), writesize);
+            Wire.endTransmission(true);
+        }
+
+        i2c_buf->pos += writesize;
+        chunksize -= writesize;
+
+        if (i2c_buf->pos >= i2c_buf->len) { // done with this work item
+            i2c_buf->len = 0;
+            i2c_buf->pos = 0;
+            i2c_work.next++;
+            if (i2c_work.next >= i2c_work.end) { // done with all work items
+                i2c_work.next = 0;
+                i2c_work.end = 0;
+                return;
+            }
+        }
+        if (chunksize <= 0) return;
     }
-
-    i2c_buf.pos += chunksize;
-    if (i2c_buf.pos >= i2c_buf.len) i2c_buf.len = 0; // done
 }
 
 
 IRAM_ATTR HAL_StatusTypeDef i2c_device_ready(void)
 {
-    if (i2c_buf.len) return HAL_BUSY;
+    if (i2c_work.end != 0) return HAL_BUSY;
 
     Wire.beginTransmission(i2c_dev_adr);
     uint8_t error = Wire.endTransmission(true);
@@ -125,9 +160,9 @@ void i2c_init(void)
     Wire.begin(I2C_SDA_IO, I2C_SCL_IO, I2C_CLOCKSPEED);
     Wire.setBufferSize(I2C_BUFFER_SIZE);
     Wire.setTimeout(2);
-    i2c_dev_adr = 0;
 
-    i2c_buf.len = 0;
+    i2c_work.next = 0;
+    i2c_work.end = 0;
 }
 
 
